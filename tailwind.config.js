616aa6c79832c33d5840fc2c14db51f41e6a98e9
lin945448-cloud <lin945448@gmail.com>
2026-07-21 17:18:47 +0800

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          50: '#f5f7ff',
+          500: '#6366f1',
+          600: '#4f46e5',
+        }
+      }
+    },
+  },
+  plugins: [],
+}
