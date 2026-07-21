05b6c7976f1e0cd5d033326dc5bc99bf30863e30
lin945448-cloud <lin945448@gmail.com>
2026-07-21 17:31:55 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,15 +5,7 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        brand: {
-          50: '#f5f7ff',
-          500: '#6366f1',
-          600: '#4f46e5',
-        }
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
